48d28c6b80f1b71cfbafc93dc9ed6b61dc344f35
shefankhan <shefanofficial@gmail.com>
2023-02-11 21:48:40 +0530

    firstSol

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -5,15 +5,32 @@ public class Order {
     private String id;
     private int deliveryTime;
 
+
+
     public Order(String id, String deliveryTime) {
 
-        // The deliveryTime has to converted from string to int and then stored in the attribute
+        this.id = id;
+        //The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.deliveryTime = Integer.parseInt(deliveryTime.substring(0,2))*60 + Integer.parseInt(deliveryTime.substring(3,5));
+
+
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
 }
